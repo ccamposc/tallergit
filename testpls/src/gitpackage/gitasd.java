1e464a9927474952275ec12d16ddba03f4fc726b
pipo <Alumno@eq36-lab1>
2014-04-04 11:58:39 -0300

    local pls

--- testpls/src/gitpackage/gitasd.java
+++ testpls/src/gitpackage/gitasd.java
@@ -4,8 +4,8 @@ public class gitasd {
 
 	public static void main(String[] args) {
 		System.out.println("segundo commit");
-		System.out.println("en la web viejex y wea");
-		
+		System.out.println("en el pc viejex ");
+		System.out.println("askjcsghfsrvbjkasasrv");
 	}
 
 	
