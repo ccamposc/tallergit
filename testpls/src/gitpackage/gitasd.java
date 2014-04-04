ce6fc819650fe65850de6ccc0ba42aa759a0194a
ccamposc <ccamposc@alumnos.ubiobio.cl>
2014-04-04 10:41:21 -0400

    online

--- testpls/src/gitpackage/gitasd.java
+++ testpls/src/gitpackage/gitasd.java
@@ -4,7 +4,7 @@ public class gitasd {
 
 	public static void main(String[] args) {
 		System.out.println("segundo commit");
-		System.out.println("en la web viejex");
+		System.out.println("en la web viejex y wea");
 		
 	}
 
